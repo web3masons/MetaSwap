093b0342329f26aa8fab851cc1bef5a3fdcff548
Chris Hitchcott <hitchcott@gmail.com>
2020-02-29 20:35:19 +0700

    Deploy to kovan and rinkeby

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,9 +1,24 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = "vanish fire cage artwork upon heart endorse web initial rain gown wear";
+
 module.exports = {
+  mnemonic,
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
     },
-  }
-};
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/7d0d81d0919f4f05b9ab6634be01ee73'),
+      network_id: 42,
+      networkCheckTimeout: 1000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/7d0d81d0919f4f05b9ab6634be01ee73'),
+      network_id: 4,
+      networkCheckTimeout: 1000000000
+    }
+  },
+}
