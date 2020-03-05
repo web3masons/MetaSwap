6aa099b2fbea4b80086d675801268561916b927a
Chris Hitchcott <hitchcott@gmail.com>
2020-03-05 12:40:23 +0700

    Social icons and meta description etc

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -2,6 +2,7 @@ import 'spectre.css'
 import '../custom.css'
 
 import App, { Container } from 'next/app'
+import Head from 'next/head'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -11,6 +12,37 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <Container>
+        <Head>
+          <title>MetaSwap</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="MetaSwap is a protocol that leverages metatransactions to enable instant, economically-trustless, 'gassless' swaps between multiple assets, EVM chains, and Lightning invoices."
+          />
+          <meta property="og:type" content="website" />
+          <meta name="og:title" property="og:title" content="MetaSwap" />
+          <meta
+            name="og:description"
+            property="og:description"
+            content="MetaSwap is a protocol that leverages metatransactions to enable instant, economically-trustless, 'gassless' swaps between multiple assets, EVM chains, and Lightning invoices."
+          />
+          <meta property="og:url" content="https://metaswap.io" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="MetaSwap" />
+          <meta
+            name="twitter:description"
+            content="MetaSwap is a protocol that leverages metatransactions to enable instant, economically-trustless, 'gassless' swaps between multiple assets, EVM chains, and Lightning invoices."
+          />
+          <meta name="twitter:site" content="https://metaswap.io" />
+          <meta name="twitter:creator" content="@hitchcott" />
+          <link rel="icon" type="image/png" href="/icon.png" />
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta property="og:image" content="https://metaswap.io/icon.png" />
+          <meta name="twitter:image" content="https://metaswap.io/icon.png" />
+        </Head>
         <div id="container">
           <div id="content">
             <Header />
