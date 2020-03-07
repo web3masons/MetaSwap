4ad116412d86d972aa4f8469c7019cc02997d5c5
Chris Hitchcott <hitchcott@gmail.com>
2020-03-07 22:03:34 +0700

    Add index page info

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,3 +1,13 @@
 export default function Index () {
-  return null
+  return (
+    <>
+      <div className="text-center">
+        <br/>
+        MetaSwap is in super-alpha pre-developer technical preview release mode.
+        <br/>
+        <br />
+        Check out <a href="https://github.com/web3masons/MetaSwap" target="_blank">github</a> for more info.
+      </div>
+    </>
+  )
 }
